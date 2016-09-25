7cd0b3db7052e08e8535dffa4762b3f00d668503
wcheung8 <we_cheung@yahoo.com>
2016-09-25 00:27:52 -0400

    add stock functionality

--- dashboard.php
+++ dashboard.php
@@ -21,12 +21,18 @@
 
 <h1 style="font-size: 400%"><center>Dashboard</center></h1>
 <div>
-    <p style="font-size: 200%;margin-left: 175px">Currently Tracking:</p><br>
-
+    
     <?php
         include("config.php");
+        
+        if(!isset($_POST['username'],$_POST['password'])) {
+            echo("<center>Error: not logged in</center>");
+            die();
+        }
         $username = mysqli_real_escape_string($db,$_POST['username']);
         $password = mysqli_real_escape_string($db,$_POST['password']);
+        
+        //get and salt password
         $query = "select salt from user where username ='".$username."';";
         if(!($result = mysqli_query($db, $query)))
         {
@@ -35,11 +41,12 @@
         $row = mysqli_fetch_array($result);
         $salt = $row['salt'];
         $hashedPass = hash("sha256", $salt.$password);
+        
+        //query for user identification
         $query = "select uid from user where username ='".$username."' and password='".$hashedPass."';";
         if(!($result = mysqli_query($db, $query)))
         {
-                echo "     Your registration FAILED\n";
-                echo "Error: " . $query . "<br>" . $db->error;
+                echo "Error";
         }
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
@@ -49,12 +56,156 @@
             die();
         }
         echo "<h2><center>Hello ". $username."</center></h2>";
+        
+        //check if stock as been added
+        $uid = $row['uid'];
+        
+        if(isset($_POST['addStock']) && $_POST['addStock'] != ""){
+            $addStock = $_POST['addStock'];
+            $query = "INSERT INTO owned
+                        VALUES ('$uid', '$addStock');";
+            if(!($result = mysqli_query($db, $query)))
+            {
+                printf("Error");
+            }
+            
+            echo("<center>Stock ".$_POST['addStock']." successfully added! </center>");
+        }
+        
+        // get stocks owned
+        $query = "select sid from owned where uid ='".$uid."';";
+        if(!($result = mysqli_query($db, $query)))
+        {
+            printf("Error");
+        }
+        $stocks = "";
+        if($row = mysqli_fetch_assoc( $result )){
+            $stocks = "'".$row['sid']."'";
+        }
+        
+        while ( $row = mysqli_fetch_assoc( $result ) )
+        {
+            $stocks = $stocks.",'".$row['sid']."'";
+        }
+        
+        
+        
         ?>
+        
+        
 
 </div>
+<p style="font-size: 200%;margin-left: 175px">Currently Tracking:</p><br>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css">
+<script src="https://cdn.datatables.net/r/bs-3.3.5/jqc-1.11.3,dt-1.10.8/datatables.min.js"></script>
+<script src="https://code.highcharts.com/stock/highstock.js"></script>
+<script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
+<script src="https://www.blackrock.com/tools/api/js/hackathon"></script>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+
+<div class="container">
+  <h4 style="text-align:center;">
+  Holdings
+  </h4>
+  <table id="holdings" class="table table-striped table-bordered" cellspacing="0" width="100%"></table>
+</div>
+<h4 style="text-align:center;">
+  Performance ($10,000 Investment)
+  </h4>
+<div id="returns" style="height: 400px; min-width: 310px; min-height:400px; display:block;"></div>
+<div class="container">
+  <h4 style="text-align:center;">
+  Risk
+  </h4>
+  <table>
+    <tr>
+        <th>Total Risk: </th>
+        <th id="totalRisk">0</th>
+    </tr>
+</table>
+</div>
+
+
+<script>
+$(function() {
+  var Aladdin = new blk.API();
+  var tickers = [<?php echo $stocks; ?>];
+  var pos = "";
+  var weight = 100/tickers.length;
+  for (var i = 0; i < tickers.length; i++) {
+        pos += tickers[i] + "~" + weight + "|";
+    }
+    pos = pos.substring(0,pos.length-1);
+  Aladdin.portfolioAnalysis({
+    calculateExposures: "true",
+    calculatePerformance: "true",
+    calculateRisk: "true",
+    calculateExpectedReturns: "true",
+    positions: pos,
+    useCache: "true"
+  }, function(data) {
+    var portfolio = data.resultMap.PORTFOLIOS[0].portfolios[0];
+    $('#holdings').DataTable({
+      data: portfolio.holdings.map(function(holding) {
+        return [holding.ticker, holding.description, holding.assetType, holding.pbRatio, holding.peRatio, holding.weight]
+      }),
+      columns: [{
+        title: 'Ticker'
+      }, {
+        title: 'Description'
+      }, {
+        title: 'Asset Type'
+      }, {
+        title: "P/B Ratio"
+    }, {
+        title: "P/E Ratio"
+    }, {
+        title: 'Weight'
+      }],
+      order: [
+        [0, 'desc']
+      ]
+    });
+    $('#returns').highcharts('StockChart', {
+      rangeSelector: {
+        selected: 5
+      },
+      series: [{
+        name: 'Portfolio',
+        data: portfolio.returns.performanceChart.map(function(point) {
+          return [point[0], point[1] * 10000]
+        }),
+        tooltip: {
+          valueDecimals: 2
+        }
+      }]
+    });
+    $("#totalRisk").text(portfolio.riskData.totalRisk);
+    var totalRisk = portfolio.riskData.totalRisk;
+  });
+});
+</script>
+
+<br>
+<hr>
+<br>
 
 <div>
-<p style="font-size: 200%;margin-left: 175px">Add Assets:</p>
+    <p style="font-size: 200%;margin-left: 175px">Add Assets:</p>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://test3.blackrock.com/tools/resources/css/api/main.css">
     <link rel="stylesheet" href="https://test3.blackrock.com/tools/resources/css/api/tables.css">
@@ -67,99 +218,105 @@
     <script src="https://www.blackrock.com/tools/api/js/hackathon"></script>
 
     <div class="container">
-      <div class="input-group">
-        <input id="search" type="search" class="form-control" placeholder="Search..." value="Blackrock">
+        <div class="input-group">
+            <input id="search" type="search" class="form-control" placeholder="Search..." value="Blackrock">
         <span class="input-group-btn">
-            <button id="submit" class="btn btn-primary" type="button">Search</button>
+            <button class="btn btn-primary" type="button">Search</button>
           </span>
-      </div>
-      <input id="rows" type="number" class="form-control" placeholder="Max Results (optional)">
-        <form>
-            <input id="add" type="button" value="Add">
+        </div>
+        <input id="rows" type="number" class="form-control" placeholder="Max Results (optional)">
+        <form name="myform" action="./dashboard.php" method="post">
+            <input type="hidden" name="addStock" value="" >
+            <input type="hidden" name="username" value="<?php echo $username ?>" >
+            <input type="hidden" name="password" value="<?php echo $password ?>" >
+            
+            <input id="submit" type="submit" value="Add Top Stock">
+            
         </form>
-      <table id="displayTable" class="table table-striped table-bordered" cellspacing="0" width="100%"></table>
+        <table id="displayTable" class="table table-striped table-bordered" cellspacing="0" width="100%"></table>
     </div>
 
     <script>
-    var tickers = new Set();
-    $(function() {
-        var Aladdin = new blk.API({});
-      var $table = $('#displayTable');
-      var $rows = $('#rows');
-      var $search = $('#search');
-      var table;
-      function submit(query, rows, skipColumns, columnOrder) {
-        if (table) {
-          table.destroy();
-                table = null;
-        }
-        $table.html('<p>Searching...</p>');
-        Aladdin.searchSecurities({
-          query: query,
-          rows: rows || 100,
-          useDefaultDocumentType: true,
-          useCache: true
-        }, function(data) {
-          var securities = data.resultMap.SEARCH_RESULTS[0].resultList;
-          var skipCols = skipColumns || [];
-          var startingColumns = columnOrder || [];
-          var columns = [];
-          /*securities.forEach(function(sec) {
-            for (var field in sec) {
-              if ((typeof sec[field] === 'string' || typeof sec[field] === 'number') && startingColumns.includes(field) && !columns.includes(field)) {
-                columns.push(field);
-              }
-            }
-          });
-          columns = startingColumns.concat(columns.sort());*/
-          columns = startingColumns;
-          var tableData = securities.map(function(sec) {
-            return columns.map(function(col) {
-              return col == 'score' ? sec[col].toFixed(3) : sec[col] || '-';
-            });
-          });
-            $("#add").click(function() {
-                var columnsTicker = ["ticker"];
-                var tableDataTicker = securities.map(function(sec) {
-                    return columnsTicker.map(function(col) {
-                        return col == 'score' ? sec[col].toFixed(3) : sec[col] || '-';
-                    });
-                });
-                if (tickers.length != 0) {
-                    tickers.add(tableDataTicker[0][0]);
-                }
-                console.log(tickers);
-            });
+        // var tickers = []; // new Set();
+        $(function() {
+            var Aladdin = new blk.API({});
+            var $table = $('#displayTable');
+            var $rows = $('#rows');
+            var $search = $('#search');
+            var table;
+            function submit(query, rows, skipColumns, columnOrder) {
                 if (table) {
                     table.destroy();
+                    table = null;
                 }
-          $table.empty();
-          table = $table.DataTable({
-            data: tableData,
-            columns: columns.map(function(col) {
-              return {
-                title: StringUtil.camelToHuman(col) + '\n(' + col + ')'
-              }
-            }),
-            order: [
-              [0, 'desc']
-            ]
-          });
+                $table.html('<p>Searching...</p>');
+                Aladdin.searchSecurities({
+                    query: query,
+                    rows: rows || 100,
+                    useDefaultDocumentType: true,
+                    useCache: true
+                }, function(data) {
+                    var securities = data.resultMap.SEARCH_RESULTS[0].resultList;
+                    var skipCols = skipColumns || [];
+                    var startingColumns = columnOrder || [];
+                    var columns = [];
+                    /*securities.forEach(function(sec) {
+                     for (var field in sec) {
+                     if ((typeof sec[field] === 'string' || typeof sec[field] === 'number') && startingColumns.includes(field) && !columns.includes(field)) {
+                     columns.push(field);
+                     }
+                     }
+                     });
+                     columns = startingColumns.concat(columns.sort());*/
+                    columns = startingColumns;
+                    var tableData = securities.map(function(sec) {
+                        return columns.map(function(col) {
+                            return col == 'score' ? sec[col].toFixed(3) : sec[col] || '-';
+                        });
+                    });
+                    $("#submit").click(function() {
+                        var columnsTicker = ["ticker"];
+                        var tableDataTicker = securities.map(function(sec) {
+                            return columnsTicker.map(function(col) {
+                                return col == 'score' ? sec[col].toFixed(3) : sec[col] || '-';
+                            });
+                        });
+                        if (tableDataTicker.length != 0) {
+                            // tickers.add(tableDataTicker[0][0]);
+                            document.myform.addStock.value = tableDataTicker[0][0];
+                            /*/!*<form id="tickerpost" action="dashboard.php" method="post" value>
+                             </form>*!/*/
+                        }
+                        console.log(tableDataTicker[0][0]);
+                    });
+                    if (table) {
+                        table.destroy();
+                    }
+                    $table.empty();
+                    table = $table.DataTable({
+                        data: tableData,
+                        columns: columns.map(function(col) {
+                            return {
+                                title: StringUtil.camelToHuman(col) + '\n(' + col + ')'
+                            }
+                        }),
+                        order: [
+                            [0, 'desc']
+                        ]
+                    });
+                });
+            }
+            var skipCols = ['@type', 'asOfDate', 'aladdinTicker', 'assetClass', 'countryCode', 'duration', 'effectiveDuration', 'expense', 'funFamilyNow', 'bcusip', 'bloombergId', 'bloombergTicker', 'country', 'fundFamilyName', 'fundId', 'gics1Sector', 'gics2IndustryGroup', 'gics3Industry', 'giccs4SubIndustry', 'gicsCode', 'inceptionDate', 'incomeYield', 'legalType', 'mappedDescription', 'mappedTicker', 'mappedType', 'marketCode', 'maturity', 'modelDuration', 'modifiedDuration', 'morningstarCategory', 'morningstarFundID', 'morningstarSecID', 'oad', 'pbRatio', 'peRatio', 'portfolioId', 'portfolioName', 'pricingCusip', 'prospectusNetExpenseRatio', 'returnOnAssets', 'returnOnEquity', 'riskCusip', 'secYield', 'secYieldEndDate', 'securityId', 'securityIdType', 'securityName', 'sedol', 'shareClassType', 'stdPerfAsOfDate', 'std'];
+            var colOrder = ['score', 'description', 'ticker', 'assetType', 'availability', 'country', 'isin',];
+            var searchTimeout;
+            function delayedSearch(delay) {
+                window.clearTimeout(searchTimeout);
+                searchTimeout = window.setTimeout(submit.bind(this, $search.val(), $rows.val(), skipCols, colOrder), delay);
+            }
+            $search.on('input', delayedSearch.bind($search[0], 250));
+            $('#submit').click(delayedSearch.bind($search[0], 0));
+            submit($search.val(), $rows.val(), skipCols, colOrder);
         });
-      }
-      var skipCols = ['@type', 'asOfDate', 'aladdinTicker', 'assetClass', 'countryCode', 'duration', 'effectiveDuration', 'expense', 'funFamilyNow', 'bcusip', 'bloombergId', 'bloombergTicker', 'country', 'fundFamilyName', 'fundId', 'gics1Sector', 'gics2IndustryGroup', 'gics3Industry', 'giccs4SubIndustry', 'gicsCode', 'inceptionDate', 'incomeYield', 'legalType', 'mappedDescription', 'mappedTicker', 'mappedType', 'marketCode', 'maturity', 'modelDuration', 'modifiedDuration', 'morningstarCategory', 'morningstarFundID', 'morningstarSecID', 'oad', 'pbRatio', 'peRatio', 'portfolioId', 'portfolioName', 'pricingCusip', 'prospectusNetExpenseRatio', 'returnOnAssets', 'returnOnEquity', 'riskCusip', 'secYield', 'secYieldEndDate', 'securityId', 'securityIdType', 'securityName', 'sedol', 'shareClassType', 'stdPerfAsOfDate', 'std'];
-      var colOrder = ['score', 'description', 'ticker', 'assetType', 'availability', 'country', 'isin',];
-      var searchTimeout;
-      function delayedSearch(delay) {
-        window.clearTimeout(searchTimeout);
-        searchTimeout = window.setTimeout(submit.bind(this, $search.val(), $rows.val(), skipCols, colOrder), delay);
-      }
-      $search.on('input', delayedSearch.bind($search[0], 250));
-      $('#submit').click(delayedSearch.bind($search[0], 0));
-      submit($search.val(), $rows.val(), skipCols, colOrder);
-    });
-    
-    
     </script>
     <!--
     <form>
@@ -188,6 +345,8 @@
     });
     </script>
 -->
+
+
 </div>
 
 
--- update.php
+++ update.php
@@ -1,104 +1,41 @@
 <?php
-
-    include("config.php");
     
-    
-    //instantiate phone number array
-    $phoneNumbers = array();
-    
-    
-    // query table here
-    foreach ($fallingStocks as $stock){
-        $sql ="select pnum 
-                from users 
-                where uid in (select uid 
-                                from owned 
-                                where sid == ".$stock.")";
-        if(!($result = mysqli_query($db, $query)))
-        {      
-
-           printf("Error: %s\n", mysqli_error($db));
-           exit(1);
-
-        } else {
-            
-            while ( $row = mysqli_fetch_assoc( $result ) )
-            {
-
-               foreach ($row as $key => $value)
-               {
-                     array_push($phoneNumbers, $value);
-               }
-
-            }
-            
-            
-            
-        }    
-    }
-    
-    //compare risk and add if below threshhold
     
     
     // sms alert all users with risk lower than wanted
     foreach ($phoneNumbers as $number){
         
+        //alert user
         
-        // Required if your envrionment does not handle autoloading
-        require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
-        // Use the REST API Client to make requests to the Twilio REST API
-        use Twilio\Rest\Client;
-        // Your Account SID and Auth Token from twilio.com/console
-        $sid = 'AC587db681d006e09ff7583b7d0f548ef5';
-        $token = '38adf22b4c2c4c2aea3f520b6355a219';
-        $client = new Client($sid, $token);
-        // Use the client to do fun stuff like send text messages!
-        $client->messages->create(
-        // the number you'd like to send the message to
-            '+1'.$number,
-            array(
-                // A Twilio phone number you purchased at twilio.com/console
-                'from' => '+16786078046 ',
-                // the body of the text message you'd like to send
-                'body' => 'fk u jonathan!'
-            )
-        );
         
     }
     
-    // close dbection
-    $db->close();
-
+    // close connection
+    $conn->close();
 ?>
 
 <?php
-
+// Required if your envrionment does not handle autoloading
+require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
+// Use the REST API Client to make requests to the Twilio REST API
+use Twilio\Rest\Client;
+// Your Account SID and Auth Token from twilio.com/console
+$sid = 'AC587db681d006e09ff7583b7d0f548ef5';
+$token = '38adf22b4c2c4c2aea3f520b6355a219';
+$client = new Client($sid, $token);
+// Use the client to do fun stuff like send text messages!
+$client->messages->create(
+// the number you'd like to send the message to
+    '+16789107883',
+    array(
+        // A Twilio phone number you purchased at twilio.com/console
+        'from' => '+16786078046 ',
+        // the body of the text message you'd like to send
+        'body' => 'Your securities have reached your risk threshhold!'
+    )
+);
 ?>
 <script>
-
-var http = require("http");
-
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Check your phone');
-}).listen(8081);
-console.log('Server running at http://127.0.0.1:8081/');
-
-var twilio = require('twilio');
-var client = twilio('AC587db681d006e09ff7583b7d0f548ef5', '38adf22b4c2c4c2aea3f520b6355a219');
- 
-// Sends the text message.
-
-client.sendMessage({
-  to: '6789107883', // My phone number
-  from: '6786078046',
-  body: 'Hello from Twilio!'
-});
-
-
 setTimeout(function () { window.location.reload(); }, 5*60*1000);
 // just show current time stamp to see time of last refresh.
 document.write(new Date());
